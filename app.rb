f3a5e069297ca4a7b551df238f0aedc831b41fbd
dnodolf1212 <dnodolf@gmail.com>
2020-07-14 17:30:28 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "italian_grinder"
+  end
+
+  get '/' do 
+    erb :index
+  end
+
+  post '/checkout' do
+    @new_item = session 
+    @new_item[:item] = params[:item]
+    @new_item[:item]
+
+   
+  end
+
+  
+
 end
