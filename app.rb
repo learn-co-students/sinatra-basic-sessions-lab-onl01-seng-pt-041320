ef103491e6c0de7a504255c608a7a4e908688bbe
chica25 <patricia@patriciaco.com>
2020-07-25 11:55:12 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,16 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-
+#set a session_secret
     configure do
         enable :sessions
         set :session_secret, "Go-go dancer"
     end
 
+#GET request to the route '/'
     get '/' do
         erb :index
     end
 
+#/checkout, should take the params from the form and add it to the session hash
     post '/checkout' do
         @item = session
         @item[:item] = params[:item]
